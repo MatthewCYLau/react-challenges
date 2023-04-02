9e060e8964a2c282acd078302484d70d597afea3
Matthew Lau <lau.cy.matthew@gmail.com>
2023-04-02 10:48:49 +0800

    feat: add styling for span

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -21,7 +21,8 @@ const DeleteFromList = () => {
   return (
     <div id="todo-list" className="paper">
       <Collapsible>
-        <p>Create a To-Do list</p>
+        <span>Create a to-do list</span>
+        <span>Click on each to-do item to remove it from list</span>
       </Collapsible>
       <input
         className="round-input"
