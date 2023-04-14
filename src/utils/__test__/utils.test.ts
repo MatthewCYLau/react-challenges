befc24d3996b8a71d6f7623d981befa4b785d324
Matthew Lau <lau.cy.matthew@gmail.com>
2023-04-14 17:57:33 +0100

    chore: updated test

--- src/utils/__test__/utils.test.ts
+++ src/utils/__test__/utils.test.ts
@@ -1,6 +1,6 @@
 import { returnPaginationLastPageStartIndex } from "../utils";
 
-it("returns the correct pagination last page start index.", () => {
+it("returns the correct pagination last page start index", () => {
   const POST_COUNT = 100;
   const PAGE_SIZE = 10;
   expect(returnPaginationLastPageStartIndex(POST_COUNT, PAGE_SIZE)).toEqual(90);
