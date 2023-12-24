8cd77a9c4dbe4fbeffa9179728d2d75d3cf75249
Matthew Lau <lau.cy.matthew@gmail.com>
2023-12-24 10:35:31 +0000

    test: fizzbuzz 15

--- src/utils/__test__/utils.test.ts
+++ src/utils/__test__/utils.test.ts
@@ -1,4 +1,8 @@
-import { returnPaginationLastPageStartIndex, fizzBuzz, asyncReturnArrayOfNumbers } from "../utils";
+import {
+  returnPaginationLastPageStartIndex,
+  fizzBuzz,
+  asyncReturnArrayOfNumbers,
+} from "../utils";
 
 it("returns the correct pagination last page start index", () => {
   const POST_COUNT = 100;
@@ -6,13 +10,31 @@ it("returns the correct pagination last page start index", () => {
   expect(returnPaginationLastPageStartIndex(POST_COUNT, PAGE_SIZE)).toEqual(90);
 });
 
-
-it("returns the correct array of FizzBuzz string", () => {
-  expect(fizzBuzz(6)).toEqual(['1', '2', 'Fizz', '4', 'Buzz', 'Fizz']);
+it("returns the correct array of FizzBuzz string (6)", () => {
+  expect(fizzBuzz(6)).toEqual(["1", "2", "Fizz", "4", "Buzz", "Fizz"]);
 });
 
+it("returns the correct array of FizzBuzz string (15)", () => {
+  expect(fizzBuzz(15)).toEqual([
+    "1",
+    "2",
+    "Fizz",
+    "4",
+    "Buzz",
+    "Fizz",
+    "7",
+    "8",
+    "Fizz",
+    "Buzz",
+    "11",
+    "Fizz",
+    "13",
+    "14",
+    "FizzBuzz",
+  ]);
+});
 
 it("returns the correct array of numbers", async () => {
   const res = await asyncReturnArrayOfNumbers();
-  expect(res).toEqual([1,2,3]);
+  expect(res).toEqual([1, 2, 3]);
 });
